6f614a21e996eb1906e45ac1e99a72f74f8f20be
Comamoca <comamoca.dev@gmail.com>
2022-11-21 21:10:18 +0900

    :tada: (/deno-packup-react) add template for packup and react

---
+++ deno-packup-react/src/components/Button.tsx
@@ -0,0 +1,8 @@
+import { React } from "../../deps.ts";
+
+export function PrimaryButton(props: {onClick: React.MouseEventHandler<HTMLButtonElement>, text: string}) {
+  return (
+    <button onClick={ props.onClick } className="btn btn-primary m-1">{ props.text }</button>
+  );
+}
+// () => setcount(count + 1)
