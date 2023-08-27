d91a9783a71b7ea2eaee2202c3e171228f37227f
Comamoca <comamoca.dev@gmail.com>
2023-08-27 16:47:41 +0900

    :tada: (./denops-sample) add denops plugin sample

---
+++ denops-sample/denops/plugin/main.ts
@@ -0,0 +1,18 @@
+import { Denops, ensure, execute, is } from "../../mod.ts";
+
+export async function main(denops: Denops): Promise<void> {
+  // ここにプラグインの処理を記載する
+  // console.log("Hello Denops!");
+
+  denops.dispatcher = {
+    async echo(text: unknown): Promise<unknown> {
+      ensure(text, is.String);
+      return await Promise.resolve(text);
+    },
+  };
+
+  await execute(
+    denops,
+    `command! -nargs=1 HelloWorldEcho echomsg denops#request('${denops.name}', 'echo', [<q-args>])`,
+  );
+}
---
+++ denops-sample/mod.ts
@@ -0,0 +1,4 @@
+export { type Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v5.0.1/helper/mod.ts";
+export { ensure } from "https://deno.land/x/unknownutil@v3.5.1/mod.ts";
+export { is } from "https://deno.land/x/unknownutil@v3.5.1/mod.ts";
