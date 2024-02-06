b437ed4095f2af803a1cac12560d4fc50e8c7794
Comamoca <comamoca.dev@gmail.com>
2024-02-06 23:51:39 +0900

    :sparkles: (rust-mlua/) Update

--- rust-mlua/src/main.rs
+++ rust-mlua/src/main.rs
@@ -1,19 +1,36 @@
-use romaji_kana_cvt_rust::cvt::RomajiCvt;
-
 use mlua::Lua;
+use romaji_kana_cvt_rust::cvt::RomajiCvt;
+use std::collections::HashMap;
 
 fn main() {
     let input = "kokodehakinomowonuidekudasai";
     let converter = RomajiCvt::new();
 
-    let _kana = converter.from_romaji(input.to_string()).unwrap();
+    let kana = converter.from_romaji(input.to_string()).unwrap();
 
     let lua = Lua::new();
 
+    lua.globals().set("kana", kana).unwrap();
+
     let joined = lua
-        .load(r#"table.concat({"a", "b", "c"}, "")"#)
+        .load(
+            r#"
+            kana
+            "#,
+        )
         .eval::<String>()
         .unwrap();
 
     println!("{}", joined);
+}
+
+fn create_dict() -> HashMap<&'static str, &'static str> {
+    let mut dict = HashMap::new();
+
+    dict.insert("きもの", "着物");
+    dict.insert("はきもの", "履物");
+    dict.insert("ぬいで", "脱いで");
+    dict.insert("ぬぐ", "脱ぐ");
+
+    return dict;
 }
