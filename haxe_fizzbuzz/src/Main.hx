2cd8abc023e10397500c1dd13d326b28aa42250e
Comamoca <comamoca.dev@gmail.com>
2024-04-01 14:29:55 +0900

    :tada: (/haxe_fizzbuz) Add Haxe example.

---
+++ haxe_fizzbuzz/src/Main.hx
@@ -0,0 +1,20 @@
+import Std;
+import Sys;
+
+class Main {
+  static function main() {
+    trace("Haxe is great!");
+
+    for (n in 0...30) Sys.println(fizzbuzz(n));
+
+  }
+
+   static function fizzbuzz(n: Int): String {
+	   return switch (n % 15) {
+	 	  case 0: "FizzBuzz";
+	 	  case 3, 6, 9, 12: "Fizz";
+	 	  case 5, 10: "Buzz";
+	 	  default: Std.string(n);
+	   }
+   }
+}
