d239c7f60cb3b8c22c130a308fdd86ce78d47e8d
Comamoca <comamoca.dev@gmail.com>
2024-04-25 11:50:15 +0900

    :fire: (/deno-packup-react) Move to scaffold.

--- deno-packup-react/src/components/Button.tsx
+++
@@ -1,8 +0,0 @@
-import { React } from "../../deps.ts";
-
-export function PrimaryButton(props: {onClick: React.MouseEventHandler<HTMLButtonElement>, text: string}) {
-  return (
-    <button onClick={ props.onClick } className="btn btn-primary m-1">{ props.text }</button>
-  );
-}
-// () => setcount(count + 1)
