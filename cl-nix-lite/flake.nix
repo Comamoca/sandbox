54b9ff6004775b30b066049c8e0f18ce8c7b0ac1
Comamoca <comamoca.dev@gmail.com>
2024-09-26 07:30:28 +0900

    :sparkles: (cl-nix-lite/flake.nix) Change to flake-parts from flake-utils.

--- cl-nix-lite/flake.nix
+++ cl-nix-lite/flake.nix
@@ -1,30 +1,64 @@
 {
   description = "Demo lispPackagesLite app using flakes";
-  inputs = { cl-nix-lite.url = "github:hraban/cl-nix-lite"; };
-  outputs = { self, nixpkgs, cl-nix-lite, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs =
-          nixpkgs.legacyPackages.${system}.extend cl-nix-lite.overlays.default;
-        stdenv = pkgs.stdenv;
+  inputs = {
+    cl-nix-lite.url = "github:hraban/cl-nix-lite";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      cl-nix-lite,
+      flake-utils,
+      flake-parts,
+      treefmt-nix,
+      systems,
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ treefmt-nix.flakeModule ];
+      systems = import inputs.systems;
 
-        test = { };
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          pkgs = nixpkgs.legacyPackages.${system}.extend cl-nix-lite.overlays.default;
+          stdenv = pkgs.stdenv;
+          app =
+            with pkgs.lispPackagesLite;
+            lispDerivation {
+              name = "hello";
+              lispSystem = "hello";
+              lispDependencies = [
+                alexandria
+                arrow-macros
+                rove
+              ];
+              src = pkgs.lib.cleanSource ./.;
+              dontStrip = true;
 
-        app = with pkgs.lispPackagesLite;
-          lispDerivation {
-            name = "hello";
-            lispSystem = "hello";
-            lispDependencies = [ alexandria arrow-macros rove ];
-            src = pkgs.lib.cleanSource ./.;
-            dontStrip = true;
+              installPhase = ''
+                install -D src/hello $out/bin/hello
+              '';
+            };
+        in
+        rec {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+            };
 
-            installPhase = ''
-              install -D src/hello $out/bin/hello
-            '';
+            settings.formatter = { };
           };
-      in {
-        checks = { inherit test; };
 
-        packages = { default = app; };
-      });
+          packages.default = app;
+        };
+    };
 }
