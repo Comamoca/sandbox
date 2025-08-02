54c46f5a0cf919863009ab0efa13a61bd20fd0b7
Comamoca <comamoca.dev@gmail.com>
2025-08-02 12:19:20 +0900

    :sparkles: (phoenix_flake) Add example Phoenix project with flake

---
+++ phoenix_flake/flake.nix
@@ -0,0 +1,99 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      systems,
+      nixpkgs,
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.git-hooks-nix.flakeModule
+        inputs.devenv.flakeModule
+      ];
+      systems = import inputs.systems;
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          stdenv = pkgs.stdenv;
+
+          git-secrets' = pkgs.writeShellApplication {
+            name = "git-secrets";
+            runtimeInputs = [ pkgs.git-secrets ];
+            text = ''
+              git secrets --scan
+            '';
+          };
+        in
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+            };
+
+            settings.formatter = { };
+          };
+
+          pre-commit = {
+            check.enable = true;
+            settings = {
+              hooks = {
+                treefmt.enable = true;
+                ripsecrets.enable = true;
+                git-secrets = {
+                  enable = true;
+                  name = "git-secrets";
+                  entry = "${git-secrets'}/bin/git-secrets";
+                  language = "system";
+                  types = [ "text" ];
+                };
+              };
+            };
+          };
+
+          devenv.shells.default = {
+            packages = with pkgs; [ nil inotify-tools beamMinimal28Packages.elixir-ls ];
+
+	    services = {
+	      postgres = {
+		enable = true;
+		initialScript = ''
+		  CREATE ROLE postgres SUPERUSER LOGIN PASSWORD 'postgres';
+		'';
+		initialDatabases = [
+		  {
+		    name = "postgres";
+		  } 
+		];
+	      };
+	    };
+
+            languages = {
+              elixir = {
+                enable = true;
+              };
+            };
+
+            enterShell = '''';
+          };
+        };
+    };
+}
